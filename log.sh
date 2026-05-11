93abc1b53924f871cbb8961c7cfccf88c4f32b8c
Vladyslav Stasenko <vlad.stasenko@knu.ua>
2026-05-11 17:22:47 +0300

    new com

---
+++ log.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+a=$(git log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)')
+
+echo "$a"
